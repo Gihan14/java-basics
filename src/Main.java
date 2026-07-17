fef88f113e0d7e04469237e2e2c0da3670c3acc2
Gihan Sandakelum <s24016483@ousl.lk>
2026-07-17 22:57:54 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        int sum = x + y;
+
+        System.out.println("X = " + x);
+        System.out.println("Y = " + y);
+        System.out.println("Sum = " + sum);
+    }
+}
---
+++ src/Values.java
@@ -0,0 +1,18 @@
+public class Values {
+    public static void main(String[] args) {
+
+    int num = 100;
+    long longNum = 1234567890L;
+    float floatNum = 12.5f;
+    double doubleNum = 45.6789;
+    char letter = 'A';
+    boolean status = true;
+
+    System.out.println("Integer : " + num);
+    System.out.println("Long    : " + longNum);
+    System.out.println("Float   : " + floatNum);
+    System.out.println("Double  : " + doubleNum);
+    System.out.println("Char    : " + letter);
+    System.out.println("Boolean : " + status);
+    }
+}
